c7459de6d591073d6a60ccc0c7f91beb35805350
Harshit Garg <hgarg9460@gmail.com>
2025-11-13 19:15:20 +0530

    Adding a mask to calcuate the index but not a lot changes in performance

--- include/RingBuffer.hpp
+++ include/RingBuffer.hpp
@@ -18,13 +18,32 @@ private:
 
   alignas(64) atomic<size_t> readIdx_;
   alignas(64) atomic<size_t> writeIdx_;
+  alignas(64) size_t mask_;
+
+  static size_t getPowerOfTwo(size_t capacity) {
+    if (capacity <= 2) {
+      return 2;
+    }
+    capacity = capacity / 2;
+
+    size_t newCapacity = 1;
+    while (capacity != 0) {
+      capacity = capacity / 2;
+      newCapacity = 2 * newCapacity;
+    }
+
+    return newCapacity;
+  }
 
 public:
   using value_type = T;
 
   RingBuffer(size_t capacity) {
-    cap_ = capacity;
+    cap_ = getPowerOfTwo(capacity);
     buff_ = new T[cap_];
+    readIdx_ = 0;
+    writeIdx_ = 0;
+    mask_ = cap_ - 1;
   }
 
   ~RingBuffer() { delete buff_; }
@@ -36,7 +55,7 @@ public:
     if (writeIdx == readIdx)
       return false;
 
-    val = buff_[readIdx % cap_];
+    val = buff_[readIdx & mask_];
     readIdx_.store(readIdx + 1, memory_order_release);
 
     return true;
@@ -49,7 +68,7 @@ public:
     if (writeIdx - readIdx == cap_)
       return false;
 
-    buff_[writeIdx % cap_] = val;
+    buff_[writeIdx & mask_] = val;
     writeIdx_.store(writeIdx + 1, memory_order_release);
 
     return true;
